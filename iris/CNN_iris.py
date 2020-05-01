454c480c846d1136f904cfc661fc87ca5faec05e
RitsukiShuto <rtk410s@gmail.com>
2020-05-01 14:55:10 +0900

    define class "CNN"

--- iris/CNN_iris.py
+++ iris/CNN_iris.py
@@ -29,3 +29,21 @@ x_train_val, x_test, t_train_val, t_test = train_test_split(
 x_train, x_val, t_train, t_val = train_test_split(
     x, t, test_size=0.3, random_state=0
 )
+
+# モデルを定義
+
+
+class CNN(chainer.Chain):
+    def __init__(self, train=True):
+        super(CNN, self).__init__(
+            conv1=L.Convolution2D(),
+            conv2=L.Convolution2D(),
+            l1=L.Linear(),
+        )
+        self.train = train
+
+    def __call__(self, x):
+        h = F.max_pooling_2d(F.relu(self.conv1(x)), 2)
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
+
+        return self.l1(h)
